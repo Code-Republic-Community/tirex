cd1ac1c1ea1cb891dfcc4f6b4af4118ddcce622f
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-10-05 17:38:22 +0400

    Update normalize.js

--- normalize/normalize.js
+++ normalize/normalize.js
@@ -1,30 +1,11 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const { ObjectId } = require("bson");
 const pos = require('pos');
 const lemmatize = require("wink-lemmatizer")
 const fs = require("fs");
 const natural = require('natural');
 const p = require('pluralize');
-const { count } = require('console');
-
-mongoose.connect('mongodb+srv://search123:search123@bigdb.robcs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-
-const URLSchema = new Schema({
-    title: String,
-    url: {
-        type: String,
-        required: true,
-    },
-    text: String,
-    dateOfupdate: Date
-})
-
-const Content = mongoose.model('content', URLSchema);
 var noisy_words = ["how", "a", "from",
     "we", "the", "what", "when", "where", "which", "with", "that", "this", "is", "where", "are", "an", "of", "in", "how", "to", "at", "by", "and", "it", "its", "her", "she", "he", "to", "as", "or", "to", "on", "am", 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'
 ];
-
 function remove_noisy_words(splited) {
     for (let i = 0; i < splited.length; ++i) {
         if (noisy_words.includes(splited[i])) {
@@ -34,7 +15,6 @@ function remove_noisy_words(splited) {
         }
     }
 }
-
 function make_word_singular(final_text) {
     let res = " ";
     let words = new pos.Lexer().lex(final_text);
@@ -51,17 +31,14 @@ function make_word_singular(final_text) {
     }
     return res;
 }
-
 function get_dictionary() {
     let dictionary = fs.readFileSync('./words.txt', 'utf8', ).split(/[\n]+/);
     return dictionary;
 }
-
 function get_programming_dictionary() {
     let programming_dictionary = fs.readFileSync('./programmingTerms.txt', 'utf8', ).split(/[\n]+/);
     return programming_dictionary;
 }
-
 function find_exact_match(word, word_array) {
     let min_distance = 10;
     let min_distcane_word = word;
@@ -77,7 +54,6 @@ function find_exact_match(word, word_array) {
     }
     return min_distcane_word;
 }
-
 function find_matched_words(word, dictionary) {
     let matched = 0;
     let matched_words = [];
@@ -106,7 +82,6 @@ function find_matched_words(word, dictionary) {
         return find_exact_match(word, matched_words);
     }
 }
-
 function lemmatize_word(final_text) {
     let res = " ";
     let words = new pos.Lexer().lex(final_text);
@@ -140,20 +115,18 @@ function lemmatize_word(final_text) {
     }
     return res;
 }
-
 function remove_apostrophe(splited) {
     for (let i = 0; i < splited.length; ++i) {
-        splited[i] = splited[i].replaceAll("’s", "");
-        splited[i] = splited[i].replaceAll("’", "");
-        splited[i] = splited[i].replaceAll("'", "");
-        splited[i] = splited[i].replaceAll("`", "");
-        splited[i] = splited[i].replaceAll(/['"]+/g, '');
-        splited[i] = splited[i].replaceAll(/['“]+/g, '');
-        splited[i] = splited[i].replaceAll(/['”]+/g, '');
+        splited[i] = splited[i].replace("'s", "");
+        splited[i] = splited[i].replace("'", "");
+        splited[i] = splited[i].replace("'", "");
+        splited[i] = splited[i].replace("`", "");
+        splited[i] = splited[i].replace(/['"]+/g, '');
+        splited[i] = splited[i].replace(/['"]+/g, '');
+        splited[i] = splited[i].replace(/['"]+/g, '');
     }
     return splited;
 }
-
 function normalize(text) {
     let lowertext = text.toLowerCase();
     let splited = lowertext.split(/[ ,&.:?–()!—/*-=|><{}_©-]+/);
@@ -170,28 +143,7 @@ function normalize(text) {
     //lemmatize text
     return lemmatize_word(make_word_singular(final_text));
 }
-
-function get_id(id) {
-    id = id.toString();
-    return id.substring(21, 45)
+module.exports =function find_and_normalize(result) {
+    let final_text = normalize(result);
+    return final_text;
 }
-
-function get_all_ids(ids) {
-    let arr_of_ids = [];
-    for (let i = 0; i < ids.length; ++i) {
-        arr_of_ids[i] = get_id(ids[i]);
-    }
-    return arr_of_ids;
-}
-
-async function find_and_normalize() {
-    let ids = [];
-    ids = await Content.find({}, { _id: 1 });
-    let array_of_ids = get_all_ids(ids);
-    for (const id of array_of_ids) {
-        const result = await Content.findById(id);
-        let final_text = (normalize(result.text));
-        await Content.findByIdAndUpdate({ _id: id }, { $set: { 'text': final_text, "dateOfupdate": new Date() } })
-    }
-}
-find_and_normalize();
